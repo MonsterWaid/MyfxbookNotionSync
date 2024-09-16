95a2c535165b33f8487d5a7f9257b0f34c1a0209
Waid <monsterwaid@protonmail.com>
2024-09-16 15:09:13 +0800

    add sameple config.py to input

---
+++ config.py
@@ -0,0 +1,12 @@
+# Myfxbook info
+
+myfxbook_account_id = 84004198
+myfxbook_email = 'your@protonmail.com'
+myfxbook_password = 'your_pass'
+
+# Notion API Credentials
+notion_api_token = 'secret_7XXXXXXXXXXXXXXXXXXXXX'
+notion_db_id = 'Find your db id via url'
+
+# Parameter to decide if we need to filter old data
+filter_old_data = True  # Set to True to filter out records before the last weekend
